752d289663125228b6bf736beb96c19c4aa5c317
JnKindle <Jn_never2013@163.com>
2018-08-13 11:25:32 +0800

    DynamicArchive
    DynamicArchive

---
+++ DynamicArchive/DynamicArchiveModel/BaseDynamicArchiveModel.h
@@ -0,0 +1,13 @@
+//
+//  BaseDynamicArchiveModel.h
+//  DynamicArchive
+//
+//  Created by Jn_Kindle on 2018/8/13.
+//  Copyright © 2018年 JnKindle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseDynamicArchiveModel : NSObject
+
+@end
